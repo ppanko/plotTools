0f70265e5f798cd80191a2a148336ee7339f5774
ppanko <ppanko@github.com>
2018-12-07 15:03:45 -0600

    Updating main script

--- R/mlPlot.R
+++ R/mlPlot.R
@@ -1,22 +1,25 @@
-mlPlot <-
-    function(y, x, nest, data, fixed.x = TRUE, save = FALSE, ...) {
+### Title:    Multilevel regression slope plotting function 
+### Author:   Pavel Panko
+### Created:  2015-FEB-01
+### Modified: 2018-DEC-07
 
+## Main ML plotting function 
+mlPlot <- function(y, x, nest, data, fixed.x = TRUE, save = FALSE, ...) {
+    ## Formula builder for fixed slope
     if(fixed.x == TRUE) {
-
         frm <- formula(paste0(y, "~ 1 + ", x, " + (1|", nest, ")"))
-        
+        ## Formula builder for random slope    
     } else if(fixed.x == FALSE) {
-        
         frm <- formula(paste0(y, "~ 1 + ", x, " + (1 +", x, "|", nest, ")"))
     }
-
+    ## Run lme4 model
     mod <- lmer(frm, data = data, REML = FALSE)
-
+    ## Get slope coefficients 
     l2coef <- coef(mod)[[1]]
     coef1 <- coef(summary(mod))
-
+    ## If saving, initialize png device
     if(save == TRUE) png(...)
-
+    ## Create ML plot
     plot(data[,x], data[,y], type = "n", ...)
     for(i in 1:nrow(l2coef)) {
         l2id <- rownames(l2coef)[i]
@@ -32,7 +35,6 @@ mlPlot <-
     totalymin <- coef1[1, 1] + coef1[2, 1]*totalxmin
     totalymax <- coef1[1, 1] + coef1[2, 1]*totalxmax
     lines(c(totalxmin, totalxmax), c(totalymin, totalymax), lwd=3, col = "lightsteelblue4")
-
+    ## If saving, close png device
     if(save == TRUE) dev.off()
-
 }
--- R/r2Plot.R
+++ R/r2Plot.R
@@ -1,35 +1,32 @@
-r2Plot <- function(r, type2 = FALSE) {
+### Title:    R2 efficiency plotting function  
+### Author:   Pavel Panko
+### Created:  2016-MAY-01
+### Modified: 2018-DEC-07
 
-    r2 <- c(r[1], diff(r))
+## Convenience function for calculating derivatives 
+derivative <- function(x, y) diff(y)/diff(x)
 
-    shifter <- function(n, x = r2) {
-        if (n == 0) -2*x
-        else ret <- c(tail(x, -n), head(x, n))
-    }
-
-    deriv2Prelim <- abs(rowSums(sapply(c(-1, 1, 0), shifter)))
-    deriv2 <- deriv2Prelim[-c(1, length(deriv2Prelim), which(deriv2Prelim == 0))]
-
-    elbows <- sapply(
-        1:length(deriv2), y = deriv2,
-        function(x, y) which(y == max(y[x:length(y)]))
-    )
-
-    drop <- elbows[which(diff(elbows) == max(diff(elbows)))]
-
-    r2 > mean(r2[-which(r2 == 0)])
+## Main r-squared plotting function
+r2Plot <- function(r2, cumulative = FALSE, ...) {
+    ## Calculate non-cumulative r-squared
+    if(cumulative)
+        r2 <- c(r2[1], diff(r2))
+    ## Calculate second derivative
+    x <- seq_along(r2)
+    d2 <- derivative(x[-1] - diff(x)/2, derivative(x, r2))
+    ## Biggest drop-off
+    elbows <- sapply(seq_along(d2), function(x) which(d2 == max(d2[x:length(d2)])))
+    drop   <- elbows[which(diff(elbows) == max(diff(elbows)))]
+    ## Average efficiency
     cntr <- abs(r2 - mean(r2))
     slide <- which(cntr == min(cntr))
-
-    if(!type2) {
-        plot(r2)
-        abline(v = drop, col = "red")
-        abline(v = which(deriv2 == max(deriv2)))
-        abline(h = r2[slide], col = "blue")
-    }
-
-    if(type2) {
-        plot(r2c)
-        abline(v = c(drop, slide), col = "red")
-    }
+    ## Create plot
+    plot(r2, ...)
+    abline(v = drop, col = "red")
+    abline(h = r2[slide], col = "blue")
+    ## Print report
+    msg <- strwrap(sprintf("Biggest drop in variance contribution happens at
+           component %i, below average contributions start after component %i", drop, slide),
+           width = 81)
+    return(msg)
 }
